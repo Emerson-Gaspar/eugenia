fad5aa3c351ba1c476bb5a00fe145e88e70eac01
Emerson-Gaspar <121451162+Emerson-Gaspar@users.noreply.github.com>
2023-05-31 03:38:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const quotes = [
 "Parabéns, meu amor! Hoje é o seu aniversário, e eu quero que saiba o quanto você é importante na minha vida. Você ilumina meus dias com seu sorriso, enche meu coração de amor e torna cada momento ao seu lado inesquecível. Desejo que este novo ano seja repleto de conquistas, saúde e felicidade. Estarei sempre aqui, ao seu lado, para celebrar cada vitória e enfrentar qualquer desafio. Te amo infinitamente!",
 "Feliz aniversário, minha amada! Hoje é um dia para celebrar não apenas mais um ano de vida, mas também a pessoa incrível que você é. Sua presença enche meu mundo de amor, alegria e paz. Cada dia ao seu lado é uma bênção, e sou grato por ter você como minha namorada. Que este novo ciclo seja repleto de amor, sucesso e realizações. Conte sempre comigo para te apoiar e te amar incondicionalmente. Parabéns, minha eterna companheira!",
 "Você é a razão do meu sorriso.",
+  "Em teus braços sou poeta<br> E na tua voz encontro a melodia<br> Juntos somos versos perfeitos<br> Nessa história de amor que se inicia<br>"
+
+"Te amarei até o fim dos tempos<br> Como as ondas beijam a praia<br> Como o sol aquece o dia<br> Teu amor é minha eterna fantasia<br>"
+
+  "Em teus abraços encontro paz<br> E em teus olhos vejo o infinito<br> Nessa jornada de amor e cumplicidade<br> Somos dois corações em um só grito"
   "Meu amor por você é infinito.",
   "Você me completa de uma forma que ninguém mais pode.",
   "Não consigo imaginar minha vida sem você.",
